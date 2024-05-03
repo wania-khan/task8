ddc48151ac79ac8b4ba2b43888bdf750bc307813
Wania Khan <waniak305@gmail.com>
2024-05-03 21:48:50 +0500

    upd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import HeaderSection from './components/headerSection.js';
 
 function App() {
--- src/components/headerSection.js
+++ src/components/headerSection.js
@@ -1,4 +1,5 @@
 import './headerSection.css';
+import React from 'react';
 import NGROK from '../assets/imgs/ngrok.png';
 
 function headerSection(){
